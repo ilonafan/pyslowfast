3fbcff09fd03f3e438f891aa1fde3644358f9827
ulwmd <ulwmd@student.kit.edu>
2024-11-24 23:06:46 +0800

    Move to dev folder

--- slowfast/dev/run_rrl.py
+++ slowfast/dev/run_rrl.py
@@ -8,7 +8,7 @@ from slowfast.utils.parser import load_config, parse_args
 
 from demo_net import demo
 from test_net import test
-from train_rrl import train
+from slowfast.dev.train_rrl import train
 from visualization import visualize
 
 
